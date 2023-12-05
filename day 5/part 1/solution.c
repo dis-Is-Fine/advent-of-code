f36ab1982667be817dcd656498aad300eae18934
dis-Is-Fine <damiankowalczyk2007@gmail.com>
2023-12-05 21:52:08 +0100

    Updated day 5 to use command line argument

--- day 5/part 1/solution.c
+++ day 5/part 1/solution.c
@@ -20,12 +20,12 @@ long int handleSeed(int seedNumber);
 
 int main(int argc, char *argv[]) {
 
-    // if(argc != 2) {
-    //     printf("Usage: %s <input_file>\n", argv[0]);
-    //     return -1;
-    // }
+    if(argc != 2) {
+        printf("Usage: %s <input_file>\n", argv[0]);
+        return -1;
+    }
 
-    fd = fopen("input.txt", "r");
+    fd = fopen(argv[1], "r");
 
     if (fd == NULL) {
         printf("File not found\n");
--- day 5/part 2/solution.c
+++ day 5/part 2/solution.c
@@ -23,12 +23,12 @@ long int getLocation(long int seed);
 
 int main(int argc, char *argv[]) {
 
-    // if(argc != 2) {
-    //     printf("Usage: %s <input_file>\n", argv[0]);
-    //     return -1;
-    // }
+    if(argc != 2) {
+        printf("Usage: %s <input_file>\n", argv[0]);
+        return -1;
+    }
 
-    fd = fopen("input.txt", "r");
+    fd = fopen(argv[1], "r");
 
     if (fd == NULL) {
         printf("File not found\n");
